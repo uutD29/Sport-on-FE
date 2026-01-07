3df7bb5c94dc06528402d6309841d7ea94d7b9ba
uhud <uhud.trb@gmail.com>
2026-01-07 09:12:37 +0700

    Update README and home components

--- app/components/Home/categories.tsx
+++ app/components/Home/categories.tsx
@@ -1,40 +1,59 @@
+import Image from "next/image";
 import Link from "next/link";
 import { FiArrowRight } from "react-icons/fi";
-import Image from "next/image";
 
-const CategoryList = [
-  { name: "Running", imgUrl: "category-running.png" },
-  { name: "Tennis", imgUrl: "category-tennis.png" },
-  { name: "Basketball", imgUrl: "category-basketball.png" },
-  { name: "Football", imgUrl: "category-football.png" },
-  { name: "Swimming", imgUrl: "category-swimming.png" },
-  { name: "Badminton", imgUrl: "category-badminton.png" },
+const categoryList = [
+  {
+    name: "Running",
+    imgUrl: "category-running.png",
+  },
+  {
+    name: "Tennis",
+    imgUrl: "category-tennis.png",
+  },
+  {
+    name: "Basketball",
+    imgUrl: "category-basketball.png",
+  },
+  {
+    name: "Football",
+    imgUrl: "category-football.png",
+  },
+  {
+    name: "Badminton",
+    imgUrl: "category-badminton.png",
+  },
+  {
+    name: "Swimming",
+    imgUrl: "category-swimming.png",
+  },
 ];
 
 const CategoriesSection = () => {
   return (
-    <section id="categories-section" className="container mx-auto pb-20">
+    <section id="category-section" className="container mx-auto pb-20">
       <div className="flex justify-between">
         <h2 className="font-bold text-2xl">Browse By Categories</h2>
-        <Link href="#" className="text-primary font-medium flex gap-2">
-          See All Categories
-          <FiArrowRight className="self-center " />
+        <Link href="#" className="flex gap-2 text-primary font-medium">
+          <span className="self-center">See All Categories</span>
+          <FiArrowRight className="self-center" />
         </Link>
       </div>
       <div className="grid grid-cols-6 gap-12 mt-8">
-        {CategoryList.map((category, index) => (
+        {categoryList.map((category, index) => (
           <div
-            className="rounded-lg bg-linear-to-r from-[#F1F1F1] to-[#F7F7F7] w-full aspect-square flex justify-center flex-col items-center"
+            className="rounded-lg bg-gradient-to-r from-[#F1F1F1] to-[#F7F7F7] w-full aspect-square flex justify-center"
             key={index}
           >
             <div className="self-center">
               <Image
                 src={`/images/categories/${category.imgUrl}`}
                 width={86}
-                height={86}
+                height="86"
                 alt={category.name}
+                className="mb-[10px]"
               />
-              <div className="text-xl font-medium text-primary text-center">
+              <div className="text-primary font-medium text-xl text-center">
                 {category.name}
               </div>
             </div>
@@ -44,4 +63,5 @@ const CategoriesSection = () => {
     </section>
   );
 };
+
 export default CategoriesSection;
--- app/components/Home/products.tsx
+++ app/components/Home/products.tsx
@@ -1,32 +1,61 @@
-import Link from "next/link";
 import Image from "next/image";
-import { FiPlus } from "react-icons/fi";
+import Link from "next/link";
 import Button from "../ui/button";
+import { FiPlus } from "react-icons/fi";
 
 const productList = [
-  { name: "SportsOn Slowlivin", category: "Running", price: 119000, imgUrl: "product-1.png" },
-  { name: "SportsOn Rockets Tennis", category: "Tennis", price: 990000, imgUrl: "product-2.png" },
-  { name: "SportsOn Hyperfast Shoes", category: "Running", price: 329000, imgUrl: "product-3.png" },
-  { name: "SportsOn HyperSoccer v2", category: "Football", price: 458000, imgUrl: "product-4.png" },
-  { name: "SportsOn Slowlivin", category: "Running", price: 119000, imgUrl: "product-5.png" },
-  { name: "SportsOn Basketball", category: "Running", price: 200000, imgUrl: "product-6.png" },
+  {
+    name: "SportsOn Product 1",
+    category: "Running",
+    price: 450000,
+    imgUrl: "product-1.png",
+  },
+  {
+    name: "SportsOn Product 2",
+    category: "Running",
+    price: 250000,
+    imgUrl: "product-1.png",
+  },
+  {
+    name: "SportsOn Product 3",
+    category: "Running",
+    price: 230000,
+    imgUrl: "product-3.png",
+  },
+  {
+    name: "SportsOn Product 4",
+    category: "Running",
+    price: 440000,
+    imgUrl: "product-4.png",
+  },
+  {
+    name: "SportsOn Product 5",
+    category: "Running",
+    price: 550000,
+    imgUrl: "product-5.png",
+  },
+  {
+    name: "SportsOn Product 6",
+    category: "Running",
+    price: 650000,
+    imgUrl: "product-6.png",
+  },
 ];
 
 const ProductsSection = () => {
   return (
     <section id="products-section" className="container mx-auto mt-32">
-      <h2 className="font-bold italic text-4xl text-center mb-11">
+      <h2 className="mb-11 text-center text-4xl font-bold italic">
         <span className="text-primary">OUR </span>PRODUCTS
       </h2>
-
       <div className="grid grid-cols-4 gap-5">
         {productList.map((product, index) => (
           <Link
             href="#"
             key={index}
-            className="p-1.5 bg-white hover:drop-shadow-xl duration-300"
+            className="duration-300 bg-white p-1.5 hover:drop-shadow-xl"
           >
-            <div className="bg-primary-light aspect-square w-full flex justify-center items-center relative">
+            <div className="relative flex aspect-square w-full items-center justify-center bg-primary-light">
               <Image
                 src={`/images/products/${product.imgUrl}`}
                 alt={product.name}
@@ -34,24 +63,20 @@ const ProductsSection = () => {
                 height={300}
                 className="aspect-square object-contain"
               />
-
-              <Button className="w-10 h-10 absolute right-3 top-3">
+              <Button className="absolute right-3 top-3 h-10 w-10 p-2!">
                 <FiPlus size={24} />
               </Button>
             </div>
-
-            <h3 className="font-medium text-lg mb-1.5 mt-4">
+            <h3 className="mt-4 mb-1.5 text-lg font-medium">
               {product.name}
             </h3>
-
-            <div className="flex justify-between mb-8">
+            <div className="mb-8 flex justify-between">
               <div className="text-gray-500">{product.category}</div>
-
               <div className="font-medium text-primary">
                 {Intl.NumberFormat("id-ID", {
                   style: "currency",
                   currency: "IDR",
-                  minimumFractionDigits: 0,
+                  maximumSignificantDigits: 3,
                 }).format(product.price)}
               </div>
             </div>
